f7c4715d69991da83bfb905bedcc2069ec9b2a8b
Marcos <marcos.b.campos14@gmail.com>
2025-01-07 23:30:24 -0300

    feat(2015): day2 - 1

---
+++ 2015/day2/1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FILE_LENGTH 8112
+
+int min(int side1, int side2, int side3) {
+  if (side1 <= side2 && side1 <= side3)
+    return side1;
+  else if (side2 <= side1 && side2 <= side3)
+    return side2;
+  return side3;
+}
+
+int surfaceArea(int length, int width, int height) {
+  int side1 = (length * width);
+  int side2 = (width * height);
+  int side3 = (height * length);
+
+  int minSide = min(side1, side2, side3);
+  return (2 * side1 + 2 * side2 + 2 * side3) + minSide;
+}
+
+int main() {
+  FILE *f = fopen("1.txt", "r");
+  char *line = NULL;
+  size_t len;
+  ssize_t read;
+  int l, w, h;
+  int ret = 0;
+
+  while ((read = getline(&line, &len, f)) != -1) {
+    sscanf(line, "%dx%dx%d", &l, &w, &h);
+    ret += surfaceArea(l, w, h);
+  }
+  printf("%d\n", ret);
+  free(line);
+}
